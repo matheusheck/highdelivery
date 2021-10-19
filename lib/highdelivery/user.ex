132fadccd3e87fe034ad8b9b64653a4f0b7b05d6
Matheus Heck <matheus@conduite.tv>
2021-10-19 16:48:50 +0200

    hash password on changeset

--- lib/highdelivery/user.ex
+++ lib/highdelivery/user.ex
@@ -2,9 +2,11 @@ defmodule Highdelivery.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Ecto.Changeset
+
   @primary_key {:id, :binary_id, autogenerate: true}
 
-  @requered_params [:age, :address, :cep, :cpf, :email, :password_hash, :name]
+  @requered_params [:age, :address, :cep, :cpf, :email, :password, :name]
 
   schema "users" do
     field :age, :integer
@@ -12,6 +14,7 @@ defmodule Highdelivery.User do
     field :cep, :string
     field :cpf, :string
     field :email, :string
+    field :password, :string, virtual: true
     field :password_hash, :string
     field :name, :string
 
@@ -29,5 +32,11 @@ defmodule Highdelivery.User do
     |> validate_format(:email, ~r/@/)
     |> unique_constraint([:email])
     |> unique_constraint([:cpf])
+    |> put_password_hash()
   end
+
+  defp put_password_hash(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Pbkdf2.add_hash(password))
+  end
+  defp put_password_hash(changeset), do: changeset
 end

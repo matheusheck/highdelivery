9240dca6944e72081828a433b8c1c3bce2a7993c
Matheus Heck <matheus@conduite.tv>
2021-10-19 14:33:39 +0200

    create users table 🤓

--- priv/repo/migrations/20211019122401_create_users_table.exs
+++ priv/repo/migrations/20211019122401_create_users_table.exs
@@ -2,6 +2,19 @@ defmodule Highdelivery.Repo.Migrations.CreateUsersTable do
   use Ecto.Migration
 
   def change do
+    create table :users do
+      add :address, :string
+      add :age, :intenger
+      add :cep, :string
+      add :cpf, :string
+      add :email, :string
+      add :password_hash, :string
+      add :name, :string
 
+      timestamps()
+    end
+
+    create unique_index(:users, [:cpf])
+    create unique_index(:users, [:email])
   end
 end

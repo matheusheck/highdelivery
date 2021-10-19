22a11defdcefc10502171f37b44b0e6a1611a0bd
Matheus Heck <matheus@conduite.tv>
2021-10-19 15:10:52 +0200

    WIP user schema

---
+++ lib/highdelivery/user.ex
@@ -0,0 +1,17 @@
+defmodule Rockelivery. User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  schema "users" do
+    field :age, :integer
+    field :address, :string
+    field :cep, :string
+    field :cpf, :string
+    field :email, :string
+    field :password_hash, :string
+    field :name, :string
+
+    timestamps()
+  end
+end

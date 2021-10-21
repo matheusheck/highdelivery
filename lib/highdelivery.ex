82b5290ef52f8d0a4db299e66fadca424ef209ee
Matheus Heck <matheus@conduite.tv>
2021-10-21 12:44:39 +0200

    rename get user _by id_

--- lib/highdelivery.ex
+++ lib/highdelivery.ex
@@ -3,5 +3,5 @@ defmodule Highdelivery do
   alias Highdelivery.Users.Get, as: UserGet
 
   defdelegate create_user(params), to: UserCreate, as: :call
-  defdelegate get_user(id), to: UserGet, as: :by_id
+  defdelegate get_user_by_id(id), to: UserGet, as: :by_id
 end

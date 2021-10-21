674bd0ed59b517f22131ce0026d8c64a82612fa3
Matheus Heck <matheus@conduite.tv>
2021-10-21 12:43:43 +0200

    facede get user by id

--- lib/highdelivery.ex
+++ lib/highdelivery.ex
@@ -1,5 +1,7 @@
 defmodule Highdelivery do
   alias Highdelivery.Users.Create, as: UserCreate
+  alias Highdelivery.Users.Get, as: UserGet
 
   defdelegate create_user(params), to: UserCreate, as: :call
+  defdelegate get_user(id), to: UserGet, as: :by_id
 end

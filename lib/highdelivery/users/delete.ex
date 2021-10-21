ef5b2913687af0d3230db74be422b0076530412b
Matheus Heck <matheus@conduite.tv>
2021-10-21 16:12:46 +0200

    delete plug doubled verifications

--- lib/highdelivery/users/delete.ex
+++ lib/highdelivery/users/delete.ex
@@ -1,15 +1,7 @@
 defmodule Highdelivery.Users.Delete do
-  alias Ecto.UUID
   alias Highdelivery.{Error, Repo, User}
 
   def call(id) do
-    case UUID.cast(id) do
-      :error -> {:error, Error.build_id_format_invalid}
-      {:ok, uuid} -> delete(uuid)
-    end
-  end
-
-  defp delete(id) do
     case Repo.get(User, id) do
       nil -> {:error, Error.build_user_not_found()}
       user -> Repo.delete(user)
--- lib/highdelivery/users/update.ex
+++ lib/highdelivery/users/update.ex
@@ -1,15 +1,7 @@
 defmodule Highdelivery.Users.Update do
-  alias Ecto.UUID
   alias Highdelivery.{Error, Repo, User}
 
   def call(%{"id" => id} = params) do
-    case UUID.cast(id) do
-      :error -> {:error, Error.build_id_format_invalid}
-      {:ok, _uuid} -> update(params)
-    end
-  end
-
-  defp update(%{"id" => id} = params) do
     case Repo.get(User, id) do
       nil -> {:error, Error.build_user_not_found()}
       user -> do_update(user, params)

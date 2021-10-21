5cc2d8e6b89ba14bd307f75d1ba535ce00d5d15a
Matheus Heck <matheus@conduite.tv>
2021-10-21 20:16:11 +0200

    test user schema

---
+++ test/highdelivery/user_test.exs
@@ -0,0 +1,69 @@
+defmodule Highdelivery.UserTest do
+  use Highdelivery.DataCase, async: true
+
+  alias Ecto.Changeset
+  alias Highdelivery.User
+
+  describe "changeset/2" do
+    test "when all params are valid, returns a valid changeset" do
+      params = %{
+        age: 27,
+        address: "Rua das bananeiras, 15",
+        cep: "12345678",
+        cpf: "12345678901",
+        email: "Matheus@banana.com",
+        password: "123456",
+        name: "Matheus"
+      }
+
+      response = User.changeset (params)
+
+      assert %Changeset{changes: %{name: "Matheus"}, valid?: true} = response
+    end
+
+    test "when updating a value, returns a valid changeset" do
+      params = %{
+        age: 27,
+        address: "Rua das bananeiras, 15",
+        cep: "12345678",
+        cpf: "12345678901",
+        email: "Matheus@banana.com",
+        password: "123456",
+        name: "Matheus"
+      }
+
+      update_params = %{name: "Matheus Heck"}
+
+      response =
+        params
+        |> User.changeset()
+        |> User.changeset(update_params)
+
+      assert %Changeset{changes: %{name: "Matheus Heck"}, valid?: true} = response
+    end
+
+    test "if any param is valid, returns a invalid changeset" do
+      params = %{
+        age: 17,
+        address: "Rua das bananeiras, 15",
+        cep: "1234567",
+        cpf: "1234567891",
+        email: "banana.com",
+        password: "123",
+        name: "Matheus"
+      }
+
+      response = User.changeset (params)
+
+      expected_response = %{
+        age: ["must be greater than or equal to 18"],
+        cep: ["should be at least 8 character(s)"],
+        cpf: ["should be at least 11 character(s)"],
+        email: ["has invalid format"],
+        password: ["should be at least 6 character(s)"]
+      }
+
+      assert errors_on(response) == expected_response
+    end
+  end
+end

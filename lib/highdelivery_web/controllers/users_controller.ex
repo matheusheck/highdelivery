0f244702554162814c8d96c0ed3ebd9ead3b2ee2
Matheus Heck <matheus@conduite.tv>
2021-10-21 13:27:35 +0200

    get user route

--- lib/highdelivery_web/controllers/users_controller.ex
+++ lib/highdelivery_web/controllers/users_controller.ex
@@ -14,4 +14,12 @@ defmodule HighdeliveryWeb.UsersController do
     end
   end
 
+  def show(conn, %{"id" => id}) do
+    with {:ok, %User{} = user} <- Highdelivery.get_user_by_id(id) do
+      conn
+      |> put_status(:ok)
+      |> render("show.json", user: user)
+    end
+  end
+
 end

111583370445d073209b3949b4ab919e54a44af8
Matheus Heck <matheus@conduite.tv>
2021-10-26 22:09:48 +0200

    WIP test user delete

--- test/highdelivery_web/controllers/user_controller_test.exs
+++ test/highdelivery_web/controllers/user_controller_test.exs
@@ -39,4 +39,18 @@ defmodule HighdeliveryWeb.UsersControllerTest do
       assert response == expected_response
     end
   end
+
+  describe "delete/2" do
+    test "when there is a user with the given id, deletes the user", %{conn: conn} do
+      id = "1e175ab5-031a-4b36-9a0c-96da070501af"
+      insert(:user)
+
+      response =
+        conn
+        |> delete(Routes.users_path(conn, :delete, id))
+        |> response(:no_content)
+
+      assert response == ""
+    end
+  end
 end
--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,6 +1,8 @@
 defmodule Highdelivery.Factory do
   use ExMachina
 
+  alias Highdelivery.User
+
   def user_params_factory do
     %{
       age: 27,
@@ -10,6 +12,19 @@ defmodule Highdelivery.Factory do
       email: "Matheus@banana.com",
       password: "123456",
       name: "Matheus"
+    }
+  end
+
+  def user_factory do
+    %User{
+      age: 27,
+      address: "Rua das bananeiras, 15",
+      cep: "12345678",
+      cpf: "12345678901",
+      email: "Matheus@banana.com",
+      password: "123456",
+      name: "Matheus",
+      id: "1e175ab5-031a-4b36-9a0c-96da070501af"
     }
   end
 end

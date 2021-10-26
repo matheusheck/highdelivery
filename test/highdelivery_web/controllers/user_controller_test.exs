cd8c3a313c18067be10eafdeb1293754211db3a3
Matheus Heck <matheus@conduite.tv>
2021-10-26 21:41:15 +0200

    test user controller

---
+++ test/highdelivery_web/controllers/user_controller_test.exs
@@ -0,0 +1,42 @@
+defmodule HighdeliveryWeb.UsersControllerTest do
+  use HighdeliveryWeb.ConnCase, async: true
+
+  import Highdelivery.Factory
+
+  describe "create/2" do
+    test "when all params are valid, creates the user", %{conn: conn} do
+      params = build(:user_params)
+
+      response =
+        conn
+        |> post(Routes.users_path(conn, :create, params))
+        |> json_response(:created)
+
+      assert %{"message" => "User created", "user" => %{"id" => _id, "name" => "Matheus"}} = response
+    end
+
+    test "when there is some error, returns the error", %{conn: conn} do
+      params = %{
+        "password" => "123456",
+        "name" => "Matheus"
+      }
+
+      response =
+        conn
+        |> post(Routes.users_path(conn, :create, params))
+        |> json_response(:bad_request)
+
+      expected_response = %{
+        "message" => %{
+          "address" => ["can't be blank"],
+          "age" => ["can't be blank"],
+          "cep" => ["can't be blank"],
+          "cpf" => ["can't be blank"],
+          "email" => ["can't be blank"]
+        }
+      }
+
+      assert response == expected_response
+    end
+  end
+end
